50c4cc7978df07df20bf871d9691e67286537819
Ola Abdallah <ola.azhary.m@gmail.com>
2023-02-22 05:33:17 -0800

    ugh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main (void) 
+/** 
+* main - a program that prints using puts
+* return: 0 success
+*/
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
