5973a53d352f894c09a1cae46365c0919b1280c9
Ola Abdallah <ola.azhary.m@gmail.com>
2023-02-23 00:50:16 -0800

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,7 @@ printf("size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
 printf("size of long int: %lu byte(s) \n", (unsigned long)sizeof(c));
 printf("size of long long int: %lu byte(s) \n", (unsigned long)sizeof(d));
 printf("size of float: %lu byte(s) \n", (unsigned long)sizeof(f));
+return (0);
 }
 
 
